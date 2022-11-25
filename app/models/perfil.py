151067c5fede8ccf51f710a092f8f59088dfb661
Gefferson Casasola <gcasasolah@uni.pe>
2022-11-25 17:26:01 -0500

    Creacion de base datos de los personajes

--- app/models/perfil.py
+++ app/models/perfil.py
@@ -1,13 +1,24 @@
-class Perfil:
-    def __init__(self,nombre, estado, especie):
-        self.nombre=nombre
-        self.estado=estado
-        self.especie=especie
-
-def to_json(self):
-        return {
-            'nombre':self.nombre,
-            'estado':self.estado,
-            'especie':self.especie,
-            
-        }
+class Personaje:
+    def __init__(self,id, name, status, species, type, gender, origin, location, image):
+        self.id=id
+        self.name=name
+        self.status=status
+        self.species=species
+        self.type=type
+        self.gender=gender
+        self.origin=origin
+        self.location=location
+        self.image=image
+        
+    def to_json(self):
+            return {
+            "id":self.id,
+            "name":self.name,
+            "status":self.status,
+            "species":self.species,
+            "type":self.type,
+            "gender":self.gender,
+            "origin":self.origin,
+            "location":self.location,
+            "image":self.image,
+            }
