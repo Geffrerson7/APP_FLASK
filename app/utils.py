c865c02c68f41737ab42005f378460227939cc79
Gefferson Casasola <gcasasolah@uni.pe>
2022-11-25 23:54:19 -0500

    Correcion de la funcion perfil y utils.py

--- app/utils.py
+++ app/utils.py
@@ -28,9 +28,3 @@ def insertar_personaje():
     lista_personajes=db.personajes.find().sort("id",-1)
     return lista_personajes
 
-def gravatar(email="hola@gmail.com", size=100, default="identicon", rating="g"):
-    url = "https://secure.gravatar.com/avatar"
-    hash = hashlib.md5(email.encode("utf-8")).hexdigest()
-    return "{url}/{hash}?s={size}&d={default}&r={rating}".format(
-        url=url, hash=hash, size=size, default=default, rating=rating
-    )
