3fec7ffabe589dc4fd3ee3f8b8413a6dfd4f561c
Raisa320 <67403486+Raisa320@users.noreply.github.com>
2022-11-25 22:32:40 -0500

    ignore

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from .config import Config
-from .routes.libro import libros_ruta
+from .routes.listado import perfil_ruta
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     Bootstrap(app)
-    app.register_blueprint(libros_ruta)
+    app.register_blueprint(perfil_ruta)
     return app
--- app/routes/listado.py
+++ app/routes/listado.py
@@ -4,22 +4,17 @@ import requests
 from app.models.perfil import Perfil
 from app.db import db
 
-@perfil_ruta.route('/', methods=['GET'])
-def index():
-         
-        url='https://rickandmortyapi.com/api/character?page=1'
-        
-        r_perfil = requests.get(url)
-        if r_perfil.ok:
-            respuestaPerfil = r_perfil.json()
-            nombre=respuestaPerfil["results"][0]["name"]
-            estado=respuestaPerfil["results"][0]["estatus"]
-            especie=respuestaPerfil["results"][0]["especies"]
-            libro_obj=Perfil(nombre,estado,especie)
-        
-        db.libros.insert_one(libro_obj.to_json())
-       
-            
-
+@perfil_ruta.route('/crear/<page>', methods=['GET'])
+def index(page):  
+    url='https://rickandmortyapi.com/api/character?page='+page
+    
+    r_perfil = requests.get(url)
+    if r_perfil.ok:
+        respuestaPerfil = r_perfil.json()
+        nombre=respuestaPerfil["results"][0]["name"]
+        estado=respuestaPerfil["results"][0]["estatus"]
+        especie=respuestaPerfil["results"][0]["especies"]
+        libro_obj=Perfil(nombre,estado,especie)
+    db.libros.insert_one(libro_obj.to_json())     
     return render_template('index.html')
 
