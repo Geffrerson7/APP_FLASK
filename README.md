b3276b9a2e34a0907cee1ace1f05f46735d7cfd9
Gefferson Casasola <gcasasolah@uni.pe>
2022-12-06 18:20:35 -0500

    Comentarios de las funciones y modificación del readme.md

--- README.md
+++ README.md
@@ -1,19 +1,28 @@
 
 # FLASK_APP_RICK_AND_MORTY
 
+
+## Descripción
 Es una aplicación que extrae los datos de los personajes 
-de la API "Rick and Morty" y los muestra ordenados por el id 
-de forma descendente.
+de la API "Rick and Morty", los almacena en la colección "personajes" 
+de la base de datos "rick_y_morty" y los muestra ordenados por el id de forma 
+descendente en la ruta raiz en grupos de 20 por cada página, en un total de 42 páginas. 
 
+También extrae los datos de los capítulos, los almacena en la colección "episodios" y muestra los datos de los personajes que aparecen en dicho capitulo en la ruta dinámica
+"capitulo/id". 
 
-## Autores
+En ambos casos, se puede acceder a mas datos de la personaje haciendo click
+en su nombre.
+## Screenshots
 
-- [Raisa320](https://www.github.com/Raisa320)
-- [Geffrerson7](https://www.github.com/Geffrerson7)
+![App Screenshot](https://drive.google.com/file/d/1CXZpqSZNdEP80mlknoS5byFLeVS11Huy/view?usp=share_link)
+
+![App Screenshot](https://drive.google.com/file/d/1G27WnLDlo4semnR-SaWDcKAijAUWB0Lf/view?usp=sharing)
+
+![App Screenshot](https://drive.google.com/file/d/13CccgGDKnsJTDHKovUP9zF-GwJM_B26v/view?usp=share_link)
 
 
 ## Variables de entorno
-
 
 Para ejecutar este proyecto, necesitas agregar las siguientes
 variables de entorno a tu archivo .env
@@ -35,3 +44,7 @@ Si han clonado un proyecto de github y quieren instalar las dependencias, pueden
 pip install -r requirements.txt
 ```
     
+## Autores
+
+- Raisa Vanessa Orellana Rios ([Raisa320](https://www.github.com/Raisa320))
+- Gefferson Max Casasola Huamancusi ([Geffrerson7](https://www.github.com/Geffrerson7))
--- app/utils.py
+++ app/utils.py
@@ -5,7 +5,12 @@ from app.db import db
 
 
 def insertar_personaje(page):
-    page = 43 - page # 42 -> 1
+    """Función que almacena los datos de los personajes ordenados de forma
+    descendente por su id y agrupados de 20 en 20 en la colección "personajes"
+    de la base de datos, si es que no está en ella y retorna una lista con los
+    datos de los personajes almacenados para luego mostrarlos en una página del
+    template index.html que tendrá 42 páginas"""
+    page = 43 - page  # 42 -> 1
     url = "https://rickandmortyapi.com/api/character?page=" + str(page)
     r_perfil = requests.get(url)
     if r_perfil.ok:
@@ -26,7 +31,7 @@ def insertar_personaje(page):
                         personaje["image"],
                         firstSeen,
                     )
-                    db.personajes.insert_one(personaje_obj.to_json()) 
+                    db.personajes.insert_one(personaje_obj.to_json())
                 ultimo = personaje["id"] + 1
     lista_personajes = (
         db.personajes.find({"id": {"$lt": ultimo}}).sort("id", -1).limit(20)
@@ -35,6 +40,9 @@ def insertar_personaje(page):
 
 
 def first_seen(arrayEpisodio: list):
+    """Función cuyo parametro es la lista de las urls de los episodios donde
+    aparece el personaje y que retorna un string con el nombre del primer
+    capítulo donde aparece el personaje"""
     urlEpisodio1 = arrayEpisodio[0]
     requestUrl = requests.get(urlEpisodio1)
     if requestUrl.ok:
@@ -44,7 +52,10 @@ def first_seen(arrayEpisodio: list):
 
 
 def insertar_episodio(id):
-    if db.episodios.find_one({"id":id}) is None:
+    """Función cuyo parámetro es el id del capítulo y almacena los datos
+    de los capitulos en la coleccion "episodios" de la base de datos,
+    si es que aun no están almacenados en ella"""
+    if db.episodios.find_one({"id": id}) is None:
         url = "https://rickandmortyapi.com/api/episode/" + str(id)
         r_episodio = requests.get(url)
         if r_episodio.ok:
@@ -62,10 +73,13 @@ def insertar_episodio(id):
 
 
 def personajes_de_episodio(id_ep):
+    """Función, cuyo parámetro es el id del capítulo, que almacena los datos de sus personajes
+    en la colección "personajes", si es que no están en la base de datos y al final retorna
+    una lista con los datos de los personajes almacenados del capítulo"""
     lista_personajes = []
     episodio = db.episodios.find_one({"id": id_ep})
     for url in episodio["characters"]:
-        id=url.split("/")[-1]
+        id = url.split("/")[-1]
         if db.personajes.find_one({"id": int(id)}) is None:
             url_x = "https://rickandmortyapi.com/api/character/" + str(id)
             r_perfil = requests.get(url_x)
@@ -73,19 +87,21 @@ def personajes_de_episodio(id_ep):
                 respuestaPerfil = r_perfil.json()
                 firstSeen = first_seen(respuestaPerfil["episode"])
                 personaje_obj = Personaje(
-                        respuestaPerfil["id"],
-                        respuestaPerfil["name"],
-                        respuestaPerfil["status"],
-                        respuestaPerfil["species"],
-                        respuestaPerfil["type"],
-                        respuestaPerfil["gender"],
-                        respuestaPerfil["origin"]["name"],
-                        respuestaPerfil["location"]["name"],
-                        respuestaPerfil["image"],
-                        firstSeen,
+                    respuestaPerfil["id"],
+                    respuestaPerfil["name"],
+                    respuestaPerfil["status"],
+                    respuestaPerfil["species"],
+                    respuestaPerfil["type"],
+                    respuestaPerfil["gender"],
+                    respuestaPerfil["origin"]["name"],
+                    respuestaPerfil["location"]["name"],
+                    respuestaPerfil["image"],
+                    firstSeen,
                 )
                 db.personajes.insert_one(personaje_obj.to_json())
-                lista_personajes.append(db.personajes.find_one({"id": respuestaPerfil["id"]}))
+                lista_personajes.append(
+                    db.personajes.find_one({"id": respuestaPerfil["id"]})
+                )
         else:
             lista_personajes.append(db.personajes.find_one({"id": int(id)}))
     return lista_personajes
